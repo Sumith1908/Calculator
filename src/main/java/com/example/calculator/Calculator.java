42281d2d1fbd4aa79a240026aedf637ee854dc23
Sumith1908 <sumith.hegde@iiitb.ac.in>
2024-02-24 16:54:50 +0530

    more operations added

--- src/main/java/com/example/calculator/Calculator.java
+++ src/main/java/com/example/calculator/Calculator.java
@@ -1,7 +1,7 @@
 package com.example.calculator;
 import java.util.Scanner;
 
-public class calculator {
+public class Calculator {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
